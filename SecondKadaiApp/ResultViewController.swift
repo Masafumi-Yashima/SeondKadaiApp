931a1d55abb01b6bb4bdebb16e415c51a3991bf8
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-01-14 23:09:49 +0900

    遷移先Veiw追加 ViewにUI部品の追加 UI部品配置決定 segueの使用

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by YashimaMasafumi on 2021/01/14.
+//  Copyright © 2021 masa.yashi. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var showname: UILabel!
+    
+    var name:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        showname.text = "こんにちは、\(name)さん"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textname: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.name = textname.text!
+    }
 
+    @IBAction func exit(_ segue: UIStoryboardSegue){
+    }
 
 }
 
