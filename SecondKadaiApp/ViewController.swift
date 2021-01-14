ade1f7a8afcacd45a3f16ada20340bfafd135951
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-01-14 21:06:40 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by YashimaMasafumi on 2021/01/14.
+//  Copyright © 2021 masa.yashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
